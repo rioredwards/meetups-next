a92d0b2fa1c10d7da644dc6ad62c4993db1af3dd
Rio Edwards <rioredwards@gmail.com>
2023-02-22 18:41:21 -0800

    Setup database: mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,13 +1,24 @@
 // Server Side Code
 // POST: /api/new-meetup
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    // connect to the database
+    const client = await MongoClient.connect(
+      "mongodb+srv://rioredwards:T4OKFV2xtSEFW3Nn@cluster0.pnosh5s.mongodb.net/?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
 
-    // store in a database
+    // insert data into the database
+    const result = meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
